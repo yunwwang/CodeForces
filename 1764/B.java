148de47d762fd1813a9632e6347558890f830b93
yunwwang <yunwwang@gmail.com>
2022-12-22 22:18:16 -0600

    Add 1764.B

--- 1764/B.java
+++ 1764/B.java
@@ -1,3 +1,32 @@
+import java.util.*;
 public class B {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int temp = 0;
+
+            for (int j = 0; j < n; j++){
+                a[j] = sc.nextInt();
+
+                if (temp != 1) {
+                    temp = gcd(a[j], temp);
+                }
+            } 
+
+            int count = a[n-1] / temp;
+
+            System.out.println(count);
+        }
+
+        sc.close();
+    }
+
+    public static int gcd(int a, int b) {
+        return b == 0 ? a : gcd (b, a % b);
+    }
 }
